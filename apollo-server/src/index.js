9b38d10f24848a56ec06c57b837afb5f42eb8a38
Andrii Racilewicz <75903440+Feulife@users.noreply.github.com>
2023-03-02 18:42:54 +0100

    add user from a JWT, validation on server

--- apollo-server/src/index.js
+++ apollo-server/src/index.js
@@ -15,14 +15,30 @@ const db = mongoose.connect(Db);
 console.info('📚 Connected to db', db?.connections?._connectionString);
 
 // get the user info from a JWT
-
+const getUser = taken => {
+  if (taken) {
+    try {
+      // return the user information from the token
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      throw new Error('Session invalid');
+    }
+  }
+};
 // Apollo server setup
 // updated to include 'validationRules'
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-
+  validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
+  context: async ({ req }) => {
+    // get the user token from the headers
+    const token = req.headers.authorization;
+    // try to retrieve a user with the token
+    const user = getUser(token);
+    // add the db models and the user to the context
+    return { models, user };
+  }
 });
 
 const { url } = await startStandaloneServer(server, {
